aabcb9a68f03e00ceaddf09d6679b4aaf34069e6
shvhqilah <fazalshahqilah@gmail.com>
2021-06-03 09:51:11 +0800

    completed pess

---
+++ Update.php
@@ -0,0 +1,249 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Police Emergency Service System</title>
+<link href="header_style.css" rel="stylesheet" type="text/css">
+<link href="content_style.css" rel="stylesheet" type="text/css">
+<link href="page_style.css" rel="stylesheet" type="text/css">
+<?php
+if (isset($_POST["btnUpdate"])){
+	
+	require_once 'db.php';
+
+	// create database connection
+	$mysqli = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+	// check connection
+	if ($mysqli->connect_errno) {
+		die("Failed to connect to MySQL: ".$mysqli->errno);
+	}
+
+	// update patrol car status
+	$sql = "UPDATE patrolcar SET patrolcar_status_id = ? WHERE patrolcar_id = ? ";
+
+	if (!($stmt = $mysqli->prepare($sql))) {
+		die("Prepare failed: ".$mysqli->errno);
+	}
+	
+	if (!$stmt->bind_param('ss', $_POST['patrolCarStatus'], $_POST['patrolCarId'])){
+		die("Binding parameters failed: ".$stmt->errno);
+	}
+	
+	if (!$stmt->execute()) {
+		die("Update patrolcar table failed: ".$stmt->errno);
+	}
+	
+	// if patrol car status is Arrived (4) then capture the time of arrival
+	if ($_POST["patrolCarStatus"] == '4'){
+		
+		$sql = "UPDATE dispatch SET time_arrived = NOW ()
+				WHERE time_arrived is NULL and patrolcar_id = ?";
+				
+		if (!($stmt = $mysqli->prepare($sql))) {
+			die("Prepare failed: ".$mysqli->errno);
+		}
+		
+		if (!$stmt->bind_param('s', $_POST['patrolCarId'])){
+			die("Binding parameters failed: ".$stmt->errno);
+		}
+		
+		if (!$stmt->execute()) {
+			die("Update dispatch table failed: ".$stmt->errno);
+		}
+		
+	} else if ($_POST["patrolCarStatus"] == '3'){   // else if patrol car status is FREE (3) then capture the time of completion
+	
+	// First, retrieve the incident ID from dispatch table handled by that patrol car 
+	$sql = "SELECT incident_id FROM dispatch WHERE time_completed IS NULL AND patrolcar_id = ?";
+	
+	if (!($stmt = $mysqli->prepare($sql))){
+		die("Prepare failed: ".$mysqli->errno);
+	}
+	
+	if (!$stmt->bind_param('s', $_POST['patrolCarId'])){
+		die("Binding parameters failed: ".$stmt->errno);
+	}
+	
+	if (!$stmt->execute()) {
+		die("Execute failed: ".$stmt->errno);
+	}
+	
+	if (!($resultset = $stmt->get_result())) {
+		die("Getting result set failed: ".$stmt->errno);
+	}
+	
+	$incidentId;
+	
+	while ($row = $resultset->fetch_assoc()) {
+		$incidentId = $row['incident_id'];
+	}
+	
+	// next update dispatch table
+	$sql = "UPDATE dispatch SET time_completed = NOW()
+				WHERE time_completed is NULL AND patrolcar_id = ?";
+	
+	if (!($stmt = $mysqli->prepare($sql))) {
+		die("Prepare failed: ".$mysqli->errno);
+	}
+	
+	if (!$stmt->bind_param('s', $_POST['patrolCarId'])){
+		die("Binding parameters failed: ".$stmt->errno);
+	}
+	
+	if (!$stmt->execute()) {
+		die("Update dispatch table failed: ".$stmt->errno);
+	}
+	
+	// last but not least, update incident table to completed (3) all patrol car attended to it are FREE now
+	
+	$sql = "UPDATE incident SET incident_status_id = '3' WHERE incident_id = '$incidentId'
+	AND NOT EXISTS (SELECT * FROM dispatch WHERE time_completed IS NULL AND incident_id = '$incidentId')";
+	
+	if (!($stmt = $mysqli->prepare($sql))) {
+		die("Prepare failed 11: ".$mysqli->errno);
+	}
+	
+	if (!$stmt->execute()) {
+		die("Update dispatch table failed: ".$stmt->errno);
+	}
+	
+	$resultset->close();
+}
+
+	$stmt->close();
+	$mysqli->close();
+	?>
+<script type="text/javascript">window.location="./logcall.php";</script>
+
+<?php } ?>
+</head>
+<body>
+<?php require_once 'nav.php'; ?>
+<br><br>
+<?php
+if (!isset($_POST["btnSearch"])){
+?>
+<form name="form1" method="post"
+	action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?> ">
+	<table class="ContentStyle">
+	<tr></tr>
+	<tr>
+		<td>Patrol Car ID :</td>
+		<td><input type="text" name="patrolCarId" id="patrolCarId"></td>
+		<td><input type="submit" name="btnSearch" id="btnSearch" value="Search"></td>
+	</tr>
+</table>
+</form>
+<?php
+} else
+{ // post back here after clicking the btnSearch button
+	require_once 'db.php';
+	
+	// create database connection
+	$mysqli = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+	
+	// check connection
+	if ($mysqli->connect_errno) {
+		die("Failed to connect to MySQL: ".$mysqli->connect_errno);
+	}
+	
+	// retrieve patrol car detail
+	$sql = "SELECT * FROM patrolcar WHERE patrolcar_id = ?";
+	
+	if (!($stmt = $mysqli->prepare($sql))){
+		die("Prepare failed: ".$mysqli->errno);
+	}
+	
+	if (!$stmt->bind_param('s', $_POST['patrolCarId'])){
+		die("Binding parameters failed: ".$stmt->errno);
+	}
+	
+	if (!$stmt->execute()) {
+		die("Execute failed: ".$stmt->errno);
+	}
+	
+	if (!($resultset = $stmt->get_result())) {
+		die("Getting result set failed: ".$stmt->errno);
+	}
+	
+	// IF THE PATROL CAR DOES NOT EXIST, REDIRECT BACK TO UPDATE.PHP
+	if ($resultset->num_rows == 0) {
+		?>
+			<script type="text/javascript">window.location="./update.php";</script>
+	<?php }
+	
+	// else if the patrol car found
+	$patrolCarId;
+	$patrolCarStatusId;
+	
+	while ($row = $resultset->fetch_assoc()) {
+		$patrolCarId = $row['patrolcar_id'];
+		$patrolCarStatusId = $row['patrolcar_status_id'];
+	}
+	
+	// retrieve from patrolcar_status table for populating the combo box
+	$sql = "SELECT * FROM patrolcar_status";
+	if (!($stmt = $mysqli->prepare($sql))) {
+		die("Prepare failed: ".$mysqli->errno);
+	}
+	
+	if (!$stmt->execute()) {
+		die("Execute failed: ".$stmt->errno);
+	}
+	
+	if (!($resultset = $stmt->get_result())) {
+		die("Getting result set failed: ".$stmt->errno);
+	}
+	
+	$patrolCarStatusArray;; // an array variable
+	
+	while ($row = $resultset->fetch_assoc()) {
+		$patrolCarStatusArray[$row['patrolcar_status_id']] = $row['patrolcar_status_desc'];
+	}
+	
+	$stmt->close();
+	
+	$resultset->close();
+	
+	$mysqli->close();
+	
+?>
+
+<form name="form2" method="post"
+	action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?> ">
+	
+	<table class="ContentStyle">
+		<tr></tr>
+		<tr>
+			<td>ID :</td>
+			<td><?php echo $patrolCarId ?>
+			<input type="hidden" name="patrolCarId" id="patrolCarId"
+			value="<?php echo $patrolCarId ?>">
+		    </td>
+		</tr>
+		<tr>
+			<td>Status :</td>
+			<td><select name="patrolCarStatus" id="patrolCarStatus">
+				<?php foreach( $patrolCarStatusArray as $key => $value){ ?>
+				<option value="<?php echo $key ?>"
+					<?php if ($key==$patrolCarStatusId) {?> selected="selected"
+					<?php }?>
+				>
+					<?php echo $value ?>
+				</option>
+				<?php } ?>
+			</select></td>
+		</tr>
+		<tr>
+			<td><input type="reset"
+				name="btnCancel" id="btnCancel" value="Reset"></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input 
+			type="submit" name="btnUpdate" id="btnUpdate" value="Update">
+			</td>
+		</tr>
+	</table>
+</form>
+
+<?php } ?>
+</body>
+</html>
---
+++ pessdb.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2021 at 09:54 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pessdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dispatch`
+--
+
+CREATE TABLE `dispatch` (
+  `incident_id` int(11) NOT NULL,
+  `patrolcar_id` varchar(10) NOT NULL,
+  `time_dispatched` datetime NOT NULL,
+  `time_arrived` datetime DEFAULT NULL,
+  `time_completed` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident`
+--
+
+CREATE TABLE `incident` (
+  `incident_id` int(11) NOT NULL,
+  `caller_name` varchar(30) NOT NULL,
+  `phone_number` varchar(10) NOT NULL,
+  `incident_type_id` varchar(3) NOT NULL,
+  `incident_location` varchar(50) NOT NULL,
+  `incident_desc` varchar(100) NOT NULL,
+  `incident_status_id` varchar(1) NOT NULL,
+  `time_called` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_status`
+--
+
+CREATE TABLE `incident_status` (
+  `incident_status_id` varchar(1) NOT NULL,
+  `incident_status_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `incident_status`
+--
+
+INSERT INTO `incident_status` (`incident_status_id`, `incident_status_desc`) VALUES
+('1', 'Pending'),
+('2', 'Dispatched'),
+('3', 'Completed'),
+('4', 'Duplicate');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_type`
+--
+
+CREATE TABLE `incident_type` (
+  `incident_type_id` varchar(3) NOT NULL,
+  `incident_type_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `incident_type`
+--
+
+INSERT INTO `incident_type` (`incident_type_id`, `incident_type_desc`) VALUES
+('010', 'Fire'),
+('020', 'Riot'),
+('030', 'Burglary'),
+('040', 'Domestic Violence'),
+('050', 'Fallen Tree'),
+('060', 'Traffic Accident'),
+('070', 'Loan Shark'),
+('999', 'Others');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar`
+--
+
+CREATE TABLE `patrolcar` (
+  `patrolcar_id` varchar(10) NOT NULL,
+  `patrolcar_status_id` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patrolcar`
+--
+
+INSERT INTO `patrolcar` (`patrolcar_id`, `patrolcar_status_id`) VALUES
+('QX1234', '3'),
+('QX4321R', '3'),
+('QX4444P', '1'),
+('QX45545', '1'),
+('QX9999H', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar_status`
+--
+
+CREATE TABLE `patrolcar_status` (
+  `patrolcar_status_id` varchar(10) NOT NULL,
+  `patrolcar_status_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patrolcar_status`
+--
+
+INSERT INTO `patrolcar_status` (`patrolcar_status_id`, `patrolcar_status_desc`) VALUES
+('1', 'Dispatched'),
+('2', 'Patrol'),
+('3', 'Free'),
+('4', 'On-site'),
+('5', 'NA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dispatch`
+--
+ALTER TABLE `dispatch`
+  ADD PRIMARY KEY (`incident_id`,`patrolcar_id`),
+  ADD KEY `patrolcar_id` (`patrolcar_id`);
+
+--
+-- Indexes for table `incident`
+--
+ALTER TABLE `incident`
+  ADD PRIMARY KEY (`incident_id`),
+  ADD KEY `incident_type_id` (`incident_type_id`,`incident_status_id`),
+  ADD KEY `incident_status_id` (`incident_status_id`);
+
+--
+-- Indexes for table `incident_status`
+--
+ALTER TABLE `incident_status`
+  ADD PRIMARY KEY (`incident_status_id`);
+
+--
+-- Indexes for table `incident_type`
+--
+ALTER TABLE `incident_type`
+  ADD PRIMARY KEY (`incident_type_id`);
+
+--
+-- Indexes for table `patrolcar`
+--
+ALTER TABLE `patrolcar`
+  ADD PRIMARY KEY (`patrolcar_id`);
+
+--
+-- Indexes for table `patrolcar_status`
+--
+ALTER TABLE `patrolcar_status`
+  ADD PRIMARY KEY (`patrolcar_status_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `incident`
+--
+ALTER TABLE `incident`
+  MODIFY `incident_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `dispatch`
+--
+ALTER TABLE `dispatch`
+  ADD CONSTRAINT `dispatch_ibfk_1` FOREIGN KEY (`incident_id`) REFERENCES `incident` (`incident_id`),
+  ADD CONSTRAINT `dispatch_ibfk_2` FOREIGN KEY (`patrolcar_id`) REFERENCES `patrolcar` (`patrolcar_id`);
+
+--
+-- Constraints for table `incident`
+--
+ALTER TABLE `incident`
+  ADD CONSTRAINT `incident_ibfk_1` FOREIGN KEY (`incident_type_id`) REFERENCES `incident_type` (`incident_type_id`),
+  ADD CONSTRAINT `incident_ibfk_2` FOREIGN KEY (`incident_status_id`) REFERENCES `incident_status` (`incident_status_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
